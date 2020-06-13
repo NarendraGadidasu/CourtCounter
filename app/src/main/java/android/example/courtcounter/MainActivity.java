798ff3cc9d71946553e9be843adf779cc549f77e
gadi <ngadidasu@gmail.com>
2020-06-13 11:56:47 -0400

    Added Comments

--- app/src/main/java/android/example/courtcounter/MainActivity.java
+++ app/src/main/java/android/example/courtcounter/MainActivity.java
@@ -5,11 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    /**Initialize score of each team to 0*/
     int scoreTeamA = 0;
     int scoreTeamB = 0;
 
@@ -21,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         displayForTeamB(scoreTeamB);
     }
 
+    /**method called when the button +3 points is pressed for Team A*/
     public void addThreeForTeamA (View view) {
 
         scoreTeamA = scoreTeamA + 3;
@@ -28,18 +27,21 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /**method called when the button +2 points is pressed for Team A*/
     public void addTwoForTeamA (View view) {
 
         scoreTeamA = scoreTeamA + 2;
         displayForTeamA(scoreTeamA);
     }
 
+    /**method called when the button Free Throw is pressed for Team A*/
     public void addOneForTeamA(View view) {
 
         scoreTeamA = scoreTeamA + 1;
         displayForTeamA(scoreTeamA);
     }
 
+    /**method called when the button +3 points is pressed for Team B*/
     public void addThreeForTeamB (View view) {
 
         scoreTeamB = scoreTeamB + 3;
@@ -47,18 +49,21 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /**method called when the button +2 points is pressed for Team B*/
     public void addTwoForTeamB (View view) {
 
         scoreTeamB = scoreTeamB + 2;
         displayForTeamB(scoreTeamB);
     }
 
+    /**method called when the button Free Throw is pressed for Team B*/
     public void addOneForTeamB(View view) {
 
         scoreTeamB = scoreTeamB + 1;
         displayForTeamB(scoreTeamB);
     }
 
+    /**method for resetting scores*/
     public void resetScores(View view) {
         scoreTeamA = 0;
         scoreTeamB = 0;
